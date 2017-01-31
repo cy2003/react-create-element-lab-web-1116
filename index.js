fb78f094a3d3f931bd8dbde11caae1c7f9bf9bd9
cy2003 <catyyee@hotmail.com>
2017-01-31 09:36:56 -0500

    done

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// /const list =
-//   React.createElement('div', {},
-//     React.createElement('h1', {}, 'My favorite ice cream flavors'),
-//     React.createElement('ul', {},
-//       [
-//         React.createElement('li', { className: 'brown' }, 'Chocolate'),
-//         React.createElement('li', { className: 'white' }, 'Vanilla'),
-//         React.createElement('li', { className: 'yellow' }, 'Banana')
-//       ]
-//     ));
-
-
 
 const meInReact =
   React.createElement('div', {className: 'me'},
